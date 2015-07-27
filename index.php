fa27894092d070ae2480df431e5e0ecdf719edff
hursey013 <hursey013@gmail.com>
2015-07-26 20:57:58 -0400

    google font tweaks

--- index.php
+++ index.php
@@ -16,7 +16,6 @@
 	<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:100,300">
 	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="assets/css/styles.css">
-	<link rel="stylesheet" href="assets/css/animate.min.css">
 	<!--[if lt IE 9]>
 <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
@@ -30,7 +29,7 @@
 			<div id="main" class="col-lg-6 col-lg-offset-3 col-md-8 col-md-offset-2">
 				<div class="row">
 					<div class="col-md-8">
-						<h1 id="logo" class="animated fadeIn"><a class="link-black" href="<?php echo SITE_URL; ?>"><span style="font-weight:300;">Smoke</span><span style="font-weight:100;">SCRN</span></a></h1>
+						<h1 id="logo"><a class="link-black" href="<?php echo SITE_URL; ?>"><span style="font-weight:300;">Smoke</span><span style="font-weight:100;">SCRN</span></a></h1>
 						<h4 id="tagline" style="font-weight:300;"><?php echo SITE_TAGLINE; ?></h4>
 					</div>
 				</div>
